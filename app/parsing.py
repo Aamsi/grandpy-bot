177614634d47d8195c6984c2708458c911a258cf
Aamsi <ismaelouali45@gmail.com>
2020-06-09 23:20:14 +0200

    Correct bad parsing

--- app/parsing.py
+++ app/parsing.py
@@ -1,3 +1,5 @@
+import re
+
 from stop_words import STOP_WORDS
 
 
@@ -5,18 +7,24 @@ class ParsingMessage():
     
     def __init__(self):
         self.stop_words = STOP_WORDS
+        self.msg = ""
     
     def parse(self, msg):
-        msg_list = msg.split(' ')
-        for word in msg_list:
-            if word in self.stop_words:
-                msg_list.remove(word)
-        msg = ' '.join(msg_list)
-        return msg
+        delimiters = ['-', ' ', "'", '.', "?", "!"]
+        reg_pattern = '|'.join(map(re.escape, delimiters))
+        msg_splitted = re.split(reg_pattern, msg)
+        msg_parsed = []
+        print(msg_splitted)
+        for word in msg_splitted:
+            if word.lower() not in self.stop_words:
+                msg_parsed.append(word)
+        self.msg = ' '.join(msg_parsed)
+        
 
 
 
 
-# parsing = ParsingMessage()
-# msg = parsing.parse("Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?")
-# print(msg)
+parsing = ParsingMessage()
+parsing.parse("Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?")
+
+print(parsing.msg)
