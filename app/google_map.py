6c9e7404c4a223daee9a511c171afe2f11d58fc9
Aamsi <ismaelouali45@gmail.com>
2020-06-18 22:40:25 +0200

    Add formatted_address to request

--- app/google_map.py
+++ app/google_map.py
@@ -1,7 +1,6 @@
 import requests
 
-
-from app import app_bot, parsing
+from app import app_bot, parsing, settings
 
 
 class MapInfo():
@@ -10,20 +9,28 @@ class MapInfo():
         self.msg_received = msg_received
         self.parse = parsing.ParsingMessage(self.msg_received)
         self.msg_parsed = self.parse.keyword 
-        self.api_key = ""
+        self.api_key = settings.GOOGLE_API_KEY
         self.url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
     
-    def get_coordinates(self):
+    def get_address_and_coord(self):
         payloads = {
             'input': self.msg_parsed,
             'inputtype': 'textquery',
-            'fields': 'geometry',
+            'fields': 'formatted_address,geometry',
             'key': self.api_key,
         }
         r = requests.get(self.url, payloads)
+        print(r.json())
         print(r.url)
-        geometry = (r.json()['candidates'][0]['geometry']['location']['lat'],
-                    r.json()['candidates'][0]['geometry']['location']['lng'])
+        if r.json()['status'] == "ZERO_RESULTS":
+            return None
+        else:
+            formatted_address = r.json()['candidates'][0]['formatted_address']
+            geometry = (r.json()['candidates'][0]['geometry']['location']['lat'],
+                        r.json()['candidates'][0]['geometry']['location']['lng'])
+            return {
+                'formatted_address': formatted_address,
+                'geometry': geometry
+            }
         
-        return geometry
 
