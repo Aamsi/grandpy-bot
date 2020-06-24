2dc10e384f0b9c11671c637baaa8c57872c346e6
Aamsi <ismaelouali45@gmail.com>
2020-06-24 22:55:35 +0200

    Modifie le traitement des donnees a retourner au front suite aux changement dans les classes

--- app/routes.py
+++ app/routes.py
@@ -15,13 +15,26 @@ def index():
 def process_message():
     msg_content = request.form['msg_content']
     if msg_content:
+        # Parse message
+        parse = parsing.ParsingMessage(msg_content)
+        print(parse.msg_parsed)
         # Get coordinates
-        coord = google_map.MapInfo(msg_content)
-        get_coord = coord.get_coordinates()
-        print("Coordinates: ", get_coord)
+        map_info = google_map.MapInfo(parse.msg_parsed)
+        place_info = map_info.get_address_and_coord()
+        try:
+            coord = place_info['geometry']
+            address = place_info['address']
+        except TypeError as err:
+            print(err)
+            return jsonify({'response': 'fail'})
         # Get info of the place
-        summary = wiki.WikiInfo(msg_content)
-        get_summary = summary.get_summary()
-        print("Summary: ", get_summary)
-        return jsonify({'msg': get_summary})
+        wiki_info = wiki.WikiInfo(address, coord[0], coord[1])
+        summary = wiki_info.get_summary()
+        if address and summary:
+            return jsonify({'response': 'success',
+                            'address': address,
+                            'summary': summary
+                        })
+        else:
+            return jsonify({'response': 'fail'})
         
