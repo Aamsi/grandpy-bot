0315174e1941cc7cb8ffbab8b5814fc7da262321
Aamsi <ismaelouali45@gmail.com>
2020-07-05 19:21:39 +0200

    Modfies front code for the map

--- app/static/index.js
+++ app/static/index.js
@@ -6,9 +6,14 @@ function ConvChat (selector) {
     this.msgReceived = "";
     this.author = "user";
     this.address = null;
-    this.summary = null; 
+    this.summary = null;
+    this.lat = null;
+    this.long = null;
+    this.mapToken = null;
+    this.map = null;
     this.botMsg = {
         firstMsg: "",
+        map: "",
         sndMsg: "",
         errorMsg: ""
     }
@@ -23,6 +28,9 @@ function ConvChat (selector) {
                 if (res.response == "success") {
                     that.address = res.address;
                     that.summary = res.summary;
+                    that.lat = res.lat;
+                    that.long = res.long;
+                    that.token = res.token;
                     that.botMsg.firstMsg = `Bien sûr mon poussin ! La voici: ${that.address}.`;
                     that.botMsg.sndMsg = `Mais t'ai-je déjà raconté l'histoire de ce quartier qui m'a vu en culottes courtes ? ${that.summary}`;
                     delete that.botMsg.errorMsg;
@@ -49,10 +57,29 @@ function ConvChat (selector) {
                 return;
             }
             for (const [key, value] of Object.entries(that.botMsg)) {
-                that.msgBlock = `<div class='col-md-3 ml-2 mb-3 msg-right'><p>${value}</p></div>`
-                that.component.prepend(that.msgBlock);
+                if (key == 'map') {
+                    that.msgBlock = `<div class='col-md-3 ml-2 mb-3 msg-right'><div id='mapid'>${value}</div></div>`;
+                    that.component.prepend(that.msgBlock);
+                    that.createMap(that.lat, that.long, that.token);
+                } else {
+                    that.msgBlock = `<div class='col-md-3 ml-2 mb-3 msg-right'><p>${value}</p></div>`
+                    that.component.prepend(that.msgBlock);
+                }
             };
         };
+    };
+
+    this.createMap = function (lat, long, token) {
+        that.botMsg.map = L.map('mapid').setView([lat, long], 10);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 25,
+            id: 'mapbox',
+            tileSize: 256,
+            zoomOffset: 0,
+            accessToken: token
+        }).addTo(that.botMsg.map);
+        L.marker([lat, long]).addTo(that.botMsg.map);
     };
 
     this.update = function () {
