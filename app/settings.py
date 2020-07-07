e277a681138f4d227b86dc2ab0291d761323a825
Aamsi <ismaelouali45@gmail.com>
2020-07-07 22:13:50 +0200

    change les settings pour prendre les variables d'environnement

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,12 @@
 import os
 
-GOOGLE_API_KEY = ""
-MAPBOX_TOKEN = ""
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+MAPBOX_TOKEN = os.environ.get('MAPBOX_TOKEN')
+
+PATH_TO_TEST_RESSOURCES = os.path.join('ressources', 'pages_wiki.json')
+
+# Uncomment when testing
+# GOOGLE_API_KEY = "12345"
 
 try:
     from app.local_settings import *
