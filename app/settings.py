e09d43bacfa4ade709ccd2ba924aa198681ddecc
Aamsi <ismaelouali45@gmail.com>
2020-07-09 19:21:40 +0200

    Change settings

--- app/settings.py
+++ app/settings.py
@@ -1,11 +1,11 @@
 import os
 
-GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
-MAPBOX_TOKEN = os.environ['MAPBOX_TOKEN']
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+MAPBOX_TOKEN = os.environ.get('MAPBOX_TOKEN')
 
 PATH_TO_TEST_RESSOURCES = os.path.join('ressources', 'pages_wiki.json')
 
 try:
-    from app.local_settings import *
+    from app.local_settings import *  # noqa: F401, F403
 except ImportError:
     print("local_settings.py n'a pas ete cree.")
