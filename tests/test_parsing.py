0bf7045a3cfa41cc8e34b6060de23fc028d8135b
Aamsi <ismaelouali45@gmail.com>
2020-07-19 20:42:46 +0200

    Documente les tests et ajoute un test si y a le mot 'adresse' dans le message

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -5,8 +5,14 @@ from app.parsing import ParsingMessage
 
 class ParsingTest(unittest.TestCase):
 
-    def test_parse(self):
-        """ Test if we correctly parsed the message """
+    def test_parse_address(self):
+        """ Test if we correctly parsed the message with 'address' in message """
         msg = "Je veux l'adresse d'OpenClassrooms"
         test_parse = ParsingMessage(msg)
-        self.assertEqual(test_parse.msg_parsed, 'veux adresse OpenClassrooms')
+        self.assertEqual(test_parse.msg_parsed, "OpenClassrooms")
+    
+    def test_parse_no_address(self):
+        """ Test if we correctly parsed the message without 'address' in message """
+        msg = "C'est ou OpenClassrooms"
+        test_parse = ParsingMessage(msg)
+        self.assertEqual(test_parse.msg_parsed, "OpenClassrooms")
